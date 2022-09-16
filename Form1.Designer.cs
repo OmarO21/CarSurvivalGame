9fddab1b162ab53874502cf5448be41ee84368a4
12544 <12544@LAPTOP-UTM7J67A>
2022-09-16 12:45:33 -0500

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,295 @@
+﻿
+namespace CarSurvivalGame
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.gamePanel = new System.Windows.Forms.Panel();
+            this.trophy = new System.Windows.Forms.PictureBox();
+            this.player = new System.Windows.Forms.PictureBox();
+            this.explosion = new System.Windows.Forms.PictureBox();
+            this.Enemy2 = new System.Windows.Forms.PictureBox();
+            this.Enemy1 = new System.Windows.Forms.PictureBox();
+            this.roadTrack2 = new System.Windows.Forms.PictureBox();
+            this.roadTrack1 = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.DistanceTrav = new System.Windows.Forms.Label();
+            this.StartButton = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.label2 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.MPH = new System.Windows.Forms.Label();
+            this.gamePanel.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.trophy)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.player)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.explosion)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Enemy2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Enemy1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.roadTrack2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.roadTrack1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // gamePanel
+            // 
+            this.gamePanel.BackColor = System.Drawing.Color.Black;
+            this.gamePanel.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.gamePanel.Controls.Add(this.trophy);
+            this.gamePanel.Controls.Add(this.player);
+            this.gamePanel.Controls.Add(this.explosion);
+            this.gamePanel.Controls.Add(this.Enemy2);
+            this.gamePanel.Controls.Add(this.Enemy1);
+            this.gamePanel.Controls.Add(this.roadTrack2);
+            this.gamePanel.Controls.Add(this.roadTrack1);
+            this.gamePanel.Controls.Add(this.label1);
+            this.gamePanel.Location = new System.Drawing.Point(12, 12);
+            this.gamePanel.Name = "gamePanel";
+            this.gamePanel.Size = new System.Drawing.Size(380, 424);
+            this.gamePanel.TabIndex = 0;
+            // 
+            // trophy
+            // 
+            this.trophy.BackColor = System.Drawing.Color.Transparent;
+            this.trophy.Image = global::CarSurvivalGame.Properties.Resources.OneStar;
+            this.trophy.Location = new System.Drawing.Point(66, 157);
+            this.trophy.Name = "trophy";
+            this.trophy.Size = new System.Drawing.Size(250, 100);
+            this.trophy.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.trophy.TabIndex = 10;
+            this.trophy.TabStop = false;
+            // 
+            // player
+            // 
+            this.player.BackColor = System.Drawing.Color.Transparent;
+            this.player.Image = global::CarSurvivalGame.Properties.Resources.Player_Car;
+            this.player.Location = new System.Drawing.Point(161, 286);
+            this.player.Name = "player";
+            this.player.Size = new System.Drawing.Size(50, 101);
+            this.player.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.player.TabIndex = 9;
+            this.player.TabStop = false;
+            // 
+            // explosion
+            // 
+            this.explosion.BackColor = System.Drawing.Color.Transparent;
+            this.explosion.Image = global::CarSurvivalGame.Properties.Resources.explosion;
+            this.explosion.Location = new System.Drawing.Point(153, 234);
+            this.explosion.Name = "explosion";
+            this.explosion.Size = new System.Drawing.Size(64, 64);
+            this.explosion.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.explosion.TabIndex = 8;
+            this.explosion.TabStop = false;
+            // 
+            // Enemy2
+            // 
+            this.Enemy2.BackColor = System.Drawing.Color.Transparent;
+            this.Enemy2.Image = global::CarSurvivalGame.Properties.Resources.Firefighter;
+            this.Enemy2.Location = new System.Drawing.Point(294, 85);
+            this.Enemy2.Name = "Enemy2";
+            this.Enemy2.Size = new System.Drawing.Size(50, 101);
+            this.Enemy2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.Enemy2.TabIndex = 7;
+            this.Enemy2.TabStop = false;
+            // 
+            // Enemy1
+            // 
+            this.Enemy1.BackColor = System.Drawing.Color.Transparent;
+            this.Enemy1.Image = global::CarSurvivalGame.Properties.Resources.PoliceCar1;
+            this.Enemy1.Location = new System.Drawing.Point(66, 15);
+            this.Enemy1.Name = "Enemy1";
+            this.Enemy1.Size = new System.Drawing.Size(49, 106);
+            this.Enemy1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.Enemy1.TabIndex = 6;
+            this.Enemy1.TabStop = false;
+            // 
+            // roadTrack2
+            // 
+            this.roadTrack2.Image = ((System.Drawing.Image)(resources.GetObject("roadTrack2.Image")));
+            this.roadTrack2.Location = new System.Drawing.Point(-3, -222);
+            this.roadTrack2.Name = "roadTrack2";
+            this.roadTrack2.Size = new System.Drawing.Size(385, 632);
+            this.roadTrack2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.roadTrack2.TabIndex = 5;
+            this.roadTrack2.TabStop = false;
+            // 
+            // roadTrack1
+            // 
+            this.roadTrack1.Image = ((System.Drawing.Image)(resources.GetObject("roadTrack1.Image")));
+            this.roadTrack1.Location = new System.Drawing.Point(-2, -623);
+            this.roadTrack1.Name = "roadTrack1";
+            this.roadTrack1.Size = new System.Drawing.Size(385, 632);
+            this.roadTrack1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.roadTrack1.TabIndex = 4;
+            this.roadTrack1.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(0, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "label1";
+            // 
+            // DistanceTrav
+            // 
+            this.DistanceTrav.AutoSize = true;
+            this.DistanceTrav.Font = new System.Drawing.Font("HP Simplified Jpan", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.DistanceTrav.Location = new System.Drawing.Point(243, 442);
+            this.DistanceTrav.Name = "DistanceTrav";
+            this.DistanceTrav.Size = new System.Drawing.Size(24, 27);
+            this.DistanceTrav.TabIndex = 1;
+            this.DistanceTrav.Text = "0";
+            // 
+            // StartButton
+            // 
+            this.StartButton.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.StartButton.Font = new System.Drawing.Font("Leelawadee", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.StartButton.ForeColor = System.Drawing.Color.DarkMagenta;
+            this.StartButton.Location = new System.Drawing.Point(130, 493);
+            this.StartButton.Name = "StartButton";
+            this.StartButton.Size = new System.Drawing.Size(117, 49);
+            this.StartButton.TabIndex = 2;
+            this.StartButton.Text = "START";
+            this.StartButton.UseVisualStyleBackColor = false;
+            this.StartButton.Click += new System.EventHandler(this.StartButton_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Palatino Linotype", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(12, 543);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(403, 110);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "Use Left and Right Key to move car.\r\n\r\nTry to avoid other cars, and survive for a" +
+    "s long as you can!\r\n\r\nGame gets faster the longer you survive!\r\n";
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 10;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("HP Simplified", 15.75F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(76, 442);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(171, 24);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Distance Traveled: ";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("HP Simplified", 15.75F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(27, 466);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(134, 24);
+            this.label4.TabIndex = 5;
+            this.label4.Text = "Current Speed:";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("HP Simplified", 15.75F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(219, 466);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(48, 24);
+            this.label5.TabIndex = 6;
+            this.label5.Text = "MPH";
+            // 
+            // MPH
+            // 
+            this.MPH.AutoSize = true;
+            this.MPH.Font = new System.Drawing.Font("HP Simplified", 15.75F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.MPH.Location = new System.Drawing.Point(182, 466);
+            this.MPH.Name = "MPH";
+            this.MPH.Size = new System.Drawing.Size(21, 24);
+            this.MPH.TabIndex = 7;
+            this.MPH.Text = "0";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.ClientSize = new System.Drawing.Size(399, 749);
+            this.Controls.Add(this.MPH);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.StartButton);
+            this.Controls.Add(this.DistanceTrav);
+            this.Controls.Add(this.gamePanel);
+            this.Name = "Form1";
+            this.Text = "Car Survival Game";
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.moveCar);
+            this.KeyUp += new System.Windows.Forms.KeyEventHandler(this.stopCar);
+            this.gamePanel.ResumeLayout(false);
+            this.gamePanel.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.trophy)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.player)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.explosion)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Enemy2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Enemy1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.roadTrack2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.roadTrack1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel gamePanel;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label DistanceTrav;
+        private System.Windows.Forms.Button StartButton;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.PictureBox roadTrack2;
+        private System.Windows.Forms.PictureBox roadTrack1;
+        private System.Windows.Forms.PictureBox player;
+        private System.Windows.Forms.PictureBox explosion;
+        private System.Windows.Forms.PictureBox Enemy2;
+        private System.Windows.Forms.PictureBox Enemy1;
+        private System.Windows.Forms.PictureBox trophy;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label MPH;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,285 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CarSurvivalGame
+{
+    public partial class Form1 : Form
+    {
+        // Global Variables used
+        int CarSpeed = 5; // this is the speed in which the Player Car moves
+        int RoadSpeed = 5; // The rate in which the road moves
+        bool CarLeft; // Used to move the car left
+        bool CarRight; // Used to move the car right
+        int TrafficSpeed; // Determines how fast the traffic moveds
+        int score; // 
+        Random rnd = new Random();
+
+        public Form1()
+        {
+            InitializeComponent();
+            ResetButton();
+        }
+        private void ResetButton() // This function is used to reset or start a new game
+        {
+            trophy.Visible = false; //  Hides the trophy image when game starts
+            StartButton.Enabled = false; // Disables the start button when the game is running.
+            explosion.Visible = false; // Hides the explosion image.
+            TrafficSpeed = 5; // this will set the traffic speed back to default
+            RoadSpeed = 5; // this will set the road speed back to default
+            score = 0; // reset score to zero
+
+            // Reset player position
+            player.Left = 161;
+            player.Top = 286;
+
+            CarLeft = false;
+            CarRight = false;
+
+            // Resets the enemy cars to default position:
+            Enemy1.Left = 66;
+            Enemy1.Top = -120;
+
+            Enemy2.Left = 294;
+            Enemy2.Top = -185;
+
+            // Reset Road to default position
+            roadTrack2.Left = -3;
+            roadTrack2.Top = -222;
+            roadTrack1.Left = -2;
+            roadTrack1.Top = -638;
+
+            //starts the game timer
+            timer1.Start();
+            
+
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            score++; // Increases our score, the longer we survive
+            DistanceTrav.Text = " " + score; // Shows our score on the distance label
+
+            roadTrack1.Top += RoadSpeed;
+            roadTrack2.Top += RoadSpeed;
+
+            // MOVING THE ROAD:
+            // The following if statements are used to move the road track. If the road tracks are moved beyond a certain 
+            // point, they will reset their position, creating an "illusion", or simply simulating an animation
+            if (roadTrack1.Top > 630)
+            {
+                roadTrack1.Top = -630;
+            }
+            if (roadTrack2.Top > 630)
+            {
+                roadTrack2.Top = -630;
+            }
+
+            // CAR MOVEMENT:
+            if (CarLeft) { player.Left -= CarSpeed; } // Moves the player to the left, CarLeft is true
+            if (CarRight) { player.Left += CarSpeed; } // Moves the player to the left, CarRight is true
+
+            // CHECKING BOUNDRIES: Used to ensure the player does not move offscreen
+            if (player.Left < 1)
+            {
+                CarLeft = false; // Prevents the car from going offscren
+            }
+            else if (player.Left + player.Width > 380)
+            {
+                CarRight = false;
+            }
+
+            // ENEMY CARS MOVEMENTS:
+            Enemy1.Top += TrafficSpeed;
+            Enemy2.Top += TrafficSpeed;
+
+            // ENEMEY RESPAWN and IMAGE CHANGING:
+
+            // once the enemy car reaches the top of the panel, they will respawn
+            if (Enemy1.Top > gamePanel.Height)
+            {
+                changeEnemy1(); // change the enemy car images, upon exiting the screen. Calls a function
+                Enemy1.Left = rnd.Next(2, 160); // Gives them a random placement, on the left side when they respawn
+                Enemy1.Top = rnd.Next(100, 200) * -1; // Gives a random placement, on the top left of the screen
+            }
+
+            if (Enemy2.Top > gamePanel.Height)
+            { 
+                changeEnemy2(); // change the enemy car images, upon exiting the screen. Calls a function
+                Enemy2.Left = rnd.Next(185, 327); // Gives them a random placement, on the right side when they respawn
+                Enemy2.Top = rnd.Next(100, 200) * -1; // Gives a random placement, on the top right of the screen
+            }
+
+            // GAME OVER CONDITION / COLLISION:
+            if (player.Bounds.IntersectsWith(Enemy1.Bounds) || player.Bounds.IntersectsWith(Enemy2.Bounds))
+            {
+                GameOver(); // Triggeres Game Over function when the player touches an enemy car
+            }
+
+            // SPEEDING UP TRAFFIC:
+            if (score > 1200 && score <2000) // If the score is over 100, but below 500
+            {
+                TrafficSpeed = 6;
+                RoadSpeed = 7;
+                MPH.Text = "60 ";
+            }
+            else if (score > 2000 && score < 2750) // If the score is over 500, but below 1000
+            {
+                TrafficSpeed = 7;
+                RoadSpeed = 8;
+                MPH.Text = "70 ";
+            }
+            else if (score > 2750) // If the score is over 1200
+            {
+                TrafficSpeed = 9;
+                RoadSpeed = 10;
+                MPH.Text = "80 ";
+            }
+        }
+
+        private void moveCar(object sender, KeyEventArgs e)
+        {
+            // LEFT KEY: If the player has pressed the left key, then the CarLeft boolean is true.
+            if(e.KeyCode == Keys.Left && player.Left > 0) 
+            {
+                CarLeft = true;
+            }
+            // RIGHT KEY: If the player has pressed the right key, then the CarRight boolean is true.
+            if(e.KeyCode == Keys.Right && player.Left + player.Width < gamePanel.Width)
+            {
+                CarRight = true;
+            }
+        }
+
+        private void stopCar(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Left) // If the LEFT key is up, set CarLeft to false
+            {
+                CarLeft = false;
+            }
+            if (e.KeyCode == Keys.Right) // If the LEFT key is up, set CarLeft to false
+            {
+                CarRight = false;
+            }
+        }
+        private void changeEnemy1()
+        {
+            int num = rnd.Next(1, 8); // Generates a random number between 1 through 8
+            // With a switch statement, we can generate another car image, based on the number given.
+
+            switch(num) // Will check which number has been generated by the rnd, and assign an image based on a number.
+            {
+                case 1:
+                    Enemy1.Image = Properties.Resources.Black_Car1;
+                    break;
+                case 2:
+                    Enemy1.Image = Properties.Resources.BlueCar1;
+                    break;
+                case 3:
+                    Enemy1.Image = Properties.Resources.bluecar2;
+                    break;
+                case 4:
+                    Enemy1.Image = Properties.Resources.Firefighter1;
+                    break;
+                case 5:
+                    Enemy1.Image = Properties.Resources.Orange_Car1;
+                    break;
+                case 6:
+                    Enemy1.Image = Properties.Resources.PoliceCar1;
+                    break;
+                case 7:
+                    Enemy1.Image = Properties.Resources.PoliceCar21;
+                    break;
+                case 8:
+                    Enemy1.Image = Properties.Resources.RedCar1;
+                    break;
+                default:
+                    break;
+
+            }
+        }
+
+        private void changeEnemy2()
+        {
+            int num = rnd.Next(1, 8); // Generates a random number between 1 through 8
+            // With a switch statement, we can generate another car image, based on the number given.
+
+            switch (num) // Will check which number has been generated by the rnd, and assign an image based on a number.
+            {
+                case 1:
+                    Enemy2.Image = Properties.Resources.Black_Car1;
+                    break;
+                case 2:
+                    Enemy2.Image = Properties.Resources.BlueCar1;
+                    break;
+                case 3:
+                    Enemy2.Image = Properties.Resources.bluecar2;
+                    break;
+                case 4:
+                    Enemy2.Image = Properties.Resources.Firefighter1;
+                    break;
+                case 5:
+                    Enemy2.Image = Properties.Resources.Orange_Car1;
+                    break;
+                case 6:
+                    Enemy2.Image = Properties.Resources.PoliceCar1;
+                    break;
+                case 7:
+                    Enemy2.Image = Properties.Resources.PoliceCar2;
+                    break;
+                case 8:
+                    Enemy2.Image = Properties.Resources.RedCar1;
+                    break;
+                default:
+                    break;
+            }
+        }
+
+        private void GameOver()
+        {
+            trophy.Visible = true;
+            timer1.Stop();
+            StartButton.Enabled = true;
+
+            explosion.Visible = true;
+            player.Controls.Add(explosion);
+            explosion.Location = new Point(-8, -5);
+            explosion.BackColor = Color.Transparent;
+            explosion.BringToFront();
+
+            if (score < 500)
+            {
+                trophy.Image = Properties.Resources.OneStar;
+            }
+            else if (score > 1000)
+            {
+                trophy.Image = Properties.Resources.TwoStar;
+            }
+            else if (score > 1500)
+            {
+                trophy.Image = Properties.Resources.ThreeStar;
+            }
+            else if (score > 2500)
+            {
+                trophy.Image = Properties.Resources.FourStar;
+            }
+            else if (score > 3000)
+            {
+                trophy.Image = Properties.Resources.FiveStar;
+            }
+
+        }
+
+        private void StartButton_Click(object sender, EventArgs e)
+        {
+            ResetButton();
+        }
+    }
+   
+}
